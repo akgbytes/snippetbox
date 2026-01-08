32f614c32076d1a2cdaa63631f142d16701ab4a6
Aman Kumar Gupta <akgbytes@gmail.com>
2026-01-08 09:37:57 +0530

    implement home page with HTML templates and static file serving

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Golang!"))
+
+	files := []string{"./ui/html/base.html", "./ui/html/home.html", "./ui/html/partials/nav.html"}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
